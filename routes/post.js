788aed5e69826b78858401db47acd34c48be7b90
hassanshahzadse <hassanshahzadvs@gmail.com>
2023-08-30 01:48:30 +0500

    bug fixes

--- routes/post.js
+++ routes/post.js
@@ -4,6 +4,6 @@ const postController = require('../controllers/postController');
 
 router.post('/createPost', postController.createPost);
 router.post('/getPost/:id', postController.getPost);
-router.post('/getAllPost/:id', postController.getAllPost);
+router.get('/getAllPost/:id', postController.getAllPost);
 
 module.exports = router;
